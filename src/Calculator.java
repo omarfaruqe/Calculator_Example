d7f420045cf381b99b496b4daf38ecddd0d7979f
Omar Faruqe <faruqe.cse@gmail.com>
2024-04-30 09:40:05 +0600

    multiplication method definition provided

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,4 +2,8 @@ public class Calculator {
     public double sum(double firstValue, double secondValue){
         return firstValue + secondValue;
     }
+
+    public double multiplication(double x, double y){
+        return x * y;
+    }
 }
